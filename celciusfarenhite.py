8821b23b4ce8f4a46fa1caf326484ae9fb1d0ab5
THODKAR <81606510+THODKAR@users.noreply.github.com>
2021-08-16 16:51:11 +0530

    Add files via upload
    program converts the number from celcius to frenhite and farenhite to celcius

---
+++ celciusfarenhite.py
@@ -0,0 +1,10 @@
+#celcius to frenhite
+f=float(input("enter the farenhite temp"))
+c=(f-32)/1.8
+print(c)
+
+
+#farenhite to celcius
+cel=float(input("enter the celcius temp"))
+faren=(1.8*c)+32
+print(faren)
